a7899f329c268e83bce7544fc4c29d0ba0d387fd
George Flanagin <me@georgeflanagin.com>
2017-11-19 09:21:22 -0500

    Added __call__ to retrieve a cursor

--- sqlitedb.py
+++ sqlitedb.py
@@ -69,25 +69,28 @@ class SQLiteDB:
         self.db = None
         self.cursor = None
 
-        self.name = db_file = fname.Fname(path_to_db)
-        gkf.tombstone("Database name is " + str(db_file))
+        db_file = fname.Fname(path_to_db)
+        self.name = str(db_file)
+        gkf.tombstone("Database name is {}".format(self.name))
 
         error_on_init = False
-        if force_new_db:
+        if force_new_db and db_file:
             try:
                 gkf.tombstone("Attempting to remove old database.")
-                os.remove(str(db_file))
+                os.remove(self.name)
+
             except OSError as e:
                 # no harm in deleting a file that does not exist.
-                gkf.tombstone("Nothing to remove")
+                gkf.tombstone("Unable to remove old database.")
+
             else:
                 gkf.tombstone("Removed.")
 
         if db_file:
-            gkf.tombstone("Attempting to open existing database " + str(db_file))
-            self.db = sqlite3.connect(str(db_file), timeout=30, isolation_level='DEFERRED')
+            gkf.tombstone("Attempting to open existing database {}".format(self.name))
+            self.db = sqlite3.connect(self.name, timeout=30, isolation_level='DEFERRED')
             self.cursor = self.db.cursor()
-            gkf.tombstone("Database opened: " + str(fname.Fname(db_file)))
+            gkf.tombstone("Database opened: {}".format(self.name))
 
             try:
                 a = [ self.db.execute(stmt) for stmt in gkf.listify(local_DDL) ]
@@ -98,11 +101,12 @@ class SQLiteDB:
                 return 
 
         try:
-            gkf.tombstone("Attempting to create database in " + str(db_file))
-            self.db = sqlite3.connect(str(db_file), timeout=30, isolation_level='DEFERRED')
-            gkf.tombstone("New database created: " + str(db_file))
+            gkf.tombstone("Attempting to create database in {}".format(self.name))
+            self.db = sqlite3.connect(self.name, timeout=30, isolation_level='DEFERRED')
+            gkf.tombstone("New database created.")
 
             try:
+                gkf.tombstone("Executing DDL statements in schema")
                 a = [ self.db.execute(stmt) for stmt in gkf.listify(schema) ]
             except sqlite3.OperationalError as e:
                 gkf.tombstone(gkf.type_and_text(e))
@@ -137,6 +141,17 @@ class SQLiteDB:
         """
 
         return self.db is not None and self.OK is True
+
+
+    def __call__(self) -> sqlite3.Cursor:
+        """
+        This is a bit of syntax sugar to get the cursor object
+        out from inside the object. The purpose is to use it
+        with the pandas library.
+        """
+
+        if not self.cursor: raise Exception('No active cursor!')
+        return self.cursor
 
 
     def keys_off(self) -> None:

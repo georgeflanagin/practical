8a6d4a8ed0bfe394d78fd0979d2024fe668dc809
George Flanagin <me@georgeflanagin.com>
2020-08-28 13:43:55 -0400

    Changed name, and added the random_file and random_string functions.

--- grandom.py
+++ grandom.py
@@ -12,6 +12,7 @@ import json
 import shlex
 import shutil
 import subprocess
+import tempfile
 
 # Credits
 __author__ = 'George Flanagin'
@@ -28,7 +29,11 @@ if sys.version_info < __required_version__:
     print("This software requires Python " + str(__required_version__))
     sys.exit(os.EX_SOFTWARE)
 
-class NIST_Beacon2:
+####
+# N
+####
+
+class NISTBeacon2:
     """
     Class wrapper for the functionality.
 
@@ -87,8 +92,56 @@ class NIST_Beacon2:
         return self.blob['pulse']['timeStamp'], self.data
 
     
-if __name__ == "__main__":
-    beacon = NIST_Beacon2()
-    print(beacon())
-    print(beacon.msg)     
+
+####
+# R
+####
+
+def random_file(name_prefix:str, *, length:int=None, break_on:str=None) -> tuple:
+    """
+    Generate a new file, with random contents, consisting of printable
+    characters.
+
+    name_prefix -- In case you want to isolate them later.
+    length -- if None, then a random length <= 1MB
+    break_on -- For some testing, perhaps you want a file of "lines."
+
+    returns -- a tuple of file_name and size.
+    """    
+    f_name = None
+    num_written = -1
+
+    file_size = length if length is not None else random.choice(range(0, 1<<20))
+    fcn_signature('random_string', file_size)
+    s = random_string(file_size, True)
+
+    if break_on is not None:
+        if isinstance(break_on, str): break_on = break_on.encode('utf-8')
+        s = s.replace(break_on, b'\n')    
+
+    try:
+        f_no, f_name = tempfile.mkstemp(suffix='.txt', prefix=name_prefix)
+        num_written = os.write(f_no, s)
+        os.close(f_no)
+    except Exception as e:
+        tombstone(str(e))
+    
+    return f_name, num_written
+    
+
+
+def random_string(length:int=10, want_bytes:bool=False, all_alpha:bool=True) -> str:
+    """
+    Random data of a given length.
+    """
+    
+    s = base64.b64encode(os.urandom(length*2))
+    if want_bytes: return s[:length]
+
+    s = s.decode('utf-8')
+    if not all_alpha: return s[:length]
+
+    t = "".join([ _ for _ in s if _.isalpha() ])[:length]
+    return t
+
 
